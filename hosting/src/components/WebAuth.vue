3e9483c996e427d728a4311b4075fec286aa5c1c
sk-mk2 <ee57042@meiji.ac.jp>
2018-11-02 19:23:35 +0900

    画像位置調整

--- hosting/src/components/WebAuth.vue
+++ hosting/src/components/WebAuth.vue
@@ -1,8 +1,17 @@
 <template>
 <div>
-    <h2>WebAuthenticationAPI</h2>
+    <h2>WebAuthenticationAPI
     <a href="https://developer.mozilla.org/ja/docs/Web/API/Web_Authentication_API">
-    <img src="../image/mdn.png">
+    <img src="../image/mdn.png" width="30" height="30" class="low-position">
     </a>
+    </h2>
 </div>
 </template>
+
+<style>
+    .low-position {
+        position: relative;
+        top: 5px;
+    }
+</style>
+
